2a37ca013b637fe02d85519e00f2745734231353
Parmvir-S <parmvir.shergill@gmail.com>
2021-07-31 22:50:39 -0700

    added search functionality to google homepage

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function feelingLucky() {
     window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 }
+
+const lucky = document.querySelector(".b2")
+lucky.addEventListener("click", feelingLucky)
+
+function getUserInput() {
+    let input = document.getElementById("search").value;
+    let bing = input.split(" ").join("-").toLowerCase();
+    let url = `https://www.google.ca/search?q=${bing}`;
+    window.location.href = url;
+}
+
+const userInput = document.querySelector(".b1")
+userInput.addEventListener("click", getUserInput)
+
+
+window.addEventListener("keydown", (event) => {
+    if (event.key == "Enter") {
+        getUserInput();
+    }
+})
+
